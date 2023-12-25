d3bb7f5fe9550bd5c00c14a5543e1762dfc5d3f3
ArthurRufi <temparthur87@gmail.com>
2023-12-25 09:57:53 -0300

    models iniciados no arquivo teste.py

--- gestaofutebol/gestaofutebol/settings.py
+++ gestaofutebol/gestaofutebol/settings.py
@@ -32,14 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
+    'teste.apps.TesteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'teste',
-    'main',
+    
 ]
 
 MIDDLEWARE = [
