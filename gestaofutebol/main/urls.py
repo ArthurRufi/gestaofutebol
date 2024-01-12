7ead0c8822accb9d152b773dbf55ee119a7e4696
ArthurRufi <temparthur87@gmail.com>
2024-01-12 16:18:33 -0300

    aisistemtour verificador de quantidade e veficador impar ou par ativados

--- gestaofutebol/main/urls.py
+++ gestaofutebol/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'main'
+
+
 urlpatterns = [
     path("", views.main, name= "main"),
     path("<int:questionid>/", views.details, name="details"),
--- gestaofutebol/utils/scriptsmain/aisistemtour.py
+++ gestaofutebol/utils/scriptsmain/aisistemtour.py
@@ -1,1 +1,35 @@
-#sistema de mini inteligencia artificial que vai pegar a quantidade de time e dividir pela quantidade igualmente, vamos treinar isso mais tarde
+#sistema de mini inteligencia artificial que vai pegar a quantidade de time e gerar um tornei compativel, vamos treinar isso mais tarde
+class Tourney():
+
+    def __init__(self):
+        impar = False
+        codigoverificadorquantidade = 0
+
+    def imparoupar(self, qtdequipes):
+        if qtdequipes % 2 == 1:
+            self.impar = True
+        elif qtdequipes % 2 == 0:
+            self.impar = False
+    
+
+
+    def verficadordequantidade(self, qtdequipes):
+        if qtdequipes < 4:
+            #modelo curto
+            pass
+        elif qtdequipes >= 4 and qtdequipes <=8:
+            #outro modelo
+            pass
+        elif qtdequipes >= 8 and qtdequipes <=16:
+            #modelo oitavas de final pode ser elegivel
+            if qtdequipes == 16:
+                #chaveamento mata-mata pode ser elegivel
+                pass
+            elif qtdequipes < 16:
+                #ver outras opções
+                pass
+        elif qtdequipes > 16:
+            #modelo pontos corridos ou mata mata personalizado
+            pass
+    
+
