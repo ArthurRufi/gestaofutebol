8b1447a0811d161bfb8a526d52ec29b65ad485dd
ArthurRufi <temparthur87@gmail.com>
2024-01-25 15:45:58 -0300

    models main, adicionado funcao dos players

--- gestaofutebol/main/models.py
+++ gestaofutebol/main/models.py
@@ -4,6 +4,7 @@ class Players(models.Model):
     playername = models.CharField(max_length = 40)
     playerage = models.DateField()
     imagem = models.IntegerField(null = True)
+    funcao = models.IntegerField(null = True)
     
     def __str__(self):
         return self.playername
