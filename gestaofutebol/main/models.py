81def24f7d334700f24a69159e8748a27c08dbb4
ArthurRufi <temparthur87@gmail.com>
2024-01-25 14:35:29 -0300

    models main iniciados, LEMBRETE tentar arrumar a nao insercao do models main

--- gestaofutebol/main/models.py
+++ gestaofutebol/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
-class Players():
-    playername : models.CharField(max_length = 40)
-    playerage : models.DateField()
+class Players(models.Model):
+    playername = models.CharField(max_length = 40)
+    playerage = models.DateField()
+    imagem = models.IntegerField(null = True)
+    
