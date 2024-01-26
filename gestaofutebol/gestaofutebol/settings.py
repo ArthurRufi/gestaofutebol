91ca6e7fed4f767904df82ddd3afeb362b0da1af
ArthurRufi <temparthur87@gmail.com>
2024-01-26 14:48:01 -0300

    css criado, buscas do settings estão corretas e bem ordenadas

--- gestaofutebol/gestaofutebol/settings.py
+++ gestaofutebol/gestaofutebol/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'gestaofutebol.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'teste/templates/html')],
+        'DIRS': [os.path.join(BASE_DIR, 'teste/templates/html'),
+                 os.path.join(BASE_DIR, 'main/templates/html')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main\\static\\main\\css')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
