bcf29669de1562a4f76f9a2acbeb833c67b6be56
ArthurRufi <temparthur87@gmail.com>
2024-01-27 16:12:53 -0300

    css bug divmaster corrigido, agora tudo está de acordo

--- gestaofutebol/gestaofutebol/settings.py
+++ gestaofutebol/gestaofutebol/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main\\static\\main\\css')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main\\static\\main\\css'),
+                    os.path.join(BASE_DIR, 'main\\static\\main\\images')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
